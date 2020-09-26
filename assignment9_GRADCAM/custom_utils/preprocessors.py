46c85485709b3756e9a1cde90d7d5f37b135ce2c
sai <sairam.subramaniam@gmail.com>
2020-09-26 20:48:29 +0530

    Added albumentations and preprocessors. Yet to be tested

---
+++ assignment9_GRADCAM/custom_utils/preprocessors.py
@@ -0,0 +1,33 @@
+
+import albumentations as alb
+import albumentations.pytorch as alb_torch
+
+
+
+def calculate_mean_std(dataloader):
+    """
+    Source: https://discuss.pytorch.org/t/computing-the-mean-and-std-of-dataset/34949/2
+    """
+    mean = 0.0
+    std = 0.0
+    for imgs, _ in datatloader:
+        batch_size = imgs.size(0)
+        imgs = imgs.view(batch_size, imgs.size(1), -1)
+        mean += imgs.mean(dim=2)
+        std += imgs.std(dim=2)
+    mean /= len(dataloader.dataset)
+    std /= len(dataloader.dataset)
+
+    return [mean, std]
+
+
+
+def best_cifar10_transforms(dataloader):
+    stats = calculate_mean_std(dataloader)
+    return alb.Compose([
+        alb.Rotate(limit=10), 
+        alb.HorizontalFlip()
+        alb_torch.ToTensor(),
+        alb.Normalize(stats)
+        ])
+
