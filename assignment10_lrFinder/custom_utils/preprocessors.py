4ad5ad17875741eed247407bf1683f27582c117c
sai <sairam.subramaniam@gmail.com>
2020-10-03 23:12:46 +0530

    Added 32 crop after cutout

--- assignment10_lrFinder/custom_utils/preprocessors.py
+++ assignment10_lrFinder/custom_utils/preprocessors.py
@@ -59,6 +59,7 @@ def best_cifar10_train_transforms(stats):
         alb.Normalize(mean=list(stats[0]), std=list(stats[1])),
         alb.PadIfNeeded(min_height=48, min_width=48, border_mode=cv2.BORDER_REPLICATE, p=1.0),
         alb.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=cutout_fill),
+        alb.CenterCrop (height=32, width=32, always_apply=False, p=1.0),
         alb_torch.transforms.ToTensor()
         ], p=1.0)
 
